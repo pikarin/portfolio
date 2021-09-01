321999cd417de50dd119612919c884ece7954037
M. Aditia Firmansyah <aditia.pikarin@gmail.com>
2021-09-01 13:44:36 +0700

    Add mobile nav menu

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,56 @@
 module.exports = {
   mode: 'jit',
   purge: [
-    './src/**/*.html',
+    './src/html/*.html',
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+      },
+      colors: {
+        primary: {
+          lighter: '#C1B5FC',
+          light: '#9582F2',
+          DEFAULT: '#6C55E0',
+          dark: '#5943CB',
+        },
+        secondary: '#100E1B',
+        body: {
+          DEFAULT: '#FBFBFE',
+          inverted: '#191627',
+        },
+        container: {
+          DEFAULT: '#FFFFFF',
+          inverted: '#211D35',
+        },
+        scroll: {
+          DEFAULT: '#716C89',
+          thumb: '#545167',
+          inverted: '#E3E2E9',
+          'thumb-inverted': '#C8C6D2',
+        }
+      },
+      textColor: {
+        base: {
+          DEFAULT: '#6D6A7C',
+          inverted: '#BCBAC4',
+        },
+        light: {
+          DEFAULT: '#A19FAD',
+          inverted: '#A19FAD',
+        },
+        title: {
+          DEFAULT: '#242329',
+          inverted: '#F2F1F3',
+        },
+        'input-inverted': '#211D35'
+      },
+      boxShadow: {
+        top: '0 -1px 4px rgba(0,0,0,.15)'
+      }
+    },
   },
   variants: {
     extend: {},
