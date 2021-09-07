c95b3619353aebaba49764a26a73c868809eb461
M. Aditia Firmansyah <aditia.pikarin@gmail.com>
2021-09-07 21:43:31 +0700

    Add services section layout

--- components/Services.vue
+++ components/Services.vue
@@ -1,9 +1,42 @@
 <template>
-  <div></div>
+  <AppSection title="Services" subtitle="My technical level">
+    <AppContainer class="grid grid-cols-2 gap-5 mt-12">
+      <div
+        v-for="service in services"
+        :key="service.title"
+        class="rounded shadow-md bg-white px-6 pb-5"
+      >
+        <span>icon-here</span>
+        <h3 class="text-xl font-bold">Backend Developer</h3>
+        <a href="#" @click.prevent.stop>View more</a>
+      </div>
+    </AppContainer>
+  </AppSection>
 </template>
 
 <script>
 export default {
   name: "Services",
+  data() {
+    return {
+      services: [
+        {
+          icon: "",
+          title: "Backend Developer",
+          link: "#",
+        },
+        {
+          icon: "",
+          title: "Frontend Developer",
+          link: "#",
+        },
+        {
+          icon: "",
+          title: "DevOps",
+          link: "#",
+        },
+      ],
+    };
+  },
 };
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -11,7 +11,7 @@
 
       <Qualification />
 
-      <Services />
+      <Services id="services" />
 
       <Portfolio />
 
