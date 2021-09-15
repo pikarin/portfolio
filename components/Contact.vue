6d90cb353136ac4468073accb6e98948732e16fc
M. Aditia Firmansyah <aditia.pikarin@gmail.com>
2021-09-15 09:17:46 +0700

    Add contact section

--- components/Contact.vue
+++ components/Contact.vue
@@ -1,5 +1,27 @@
 <template>
-  <div></div>
+  <AppSection class="pb-8" title="Contact Me" subtitle="Get in touch">
+    <AppContainer class="mt-12">
+      <div class="flex items-start">
+        <IconEnvelope class="h-8 w-8 text-primary" />
+        <div class="ml-4">
+          <h3>Email</h3>
+          <p class="mt-1">aditia.pikarin@gmail.com</p>
+        </div>
+      </div>
+
+      <div class="mt-8 flex items-start">
+        <IconTwitter class="h-8 w-8 text-primary" />
+        <div class="ml-4">
+          <h3>Twitter</h3>
+          <p class="mt-1">
+            <a href="https://twitter.com/AditiaPikarin" target="_blank">
+              @AditiaPikarin
+            </a>
+          </p>
+        </div>
+      </div>
+    </AppContainer>
+  </AppSection>
 </template>
 
 <script>
