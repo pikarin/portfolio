871029ce3373bb5ce512854318c90ed31f65e69c
M. Aditia Firmansyah <aditia.pikarin@gmail.com>
2021-10-19 14:49:20 +0700

    Updates

--- _nuxt/static/1634628911/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fportfolio\u002F_nuxt\u002Fstatic\u002F1634628911",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fportfolio\u002F",assetsPath:"\u002Fportfolio\u002F_nuxt\u002F",cdnURL:a}}}}(null));
