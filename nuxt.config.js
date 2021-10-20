a06b7968bcec860ee602f66ff85751da6468ad20
M. Aditia Firmansyah <aditia.pikarin@gmail.com>
2021-10-20 12:37:11 +0700

    use gh-pages package

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,9 +2,13 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
 
+  router: {
+    base: '/portfolio'
+  },
+
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'portfolio',
+    title: 'M. Aditia Firmansyah',
     htmlAttrs: {
       lang: 'en'
     },
