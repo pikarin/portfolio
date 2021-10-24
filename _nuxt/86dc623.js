b2e7ba712dc9f725efc1bac16b7e4e36e21186b8
M. Aditia Firmansyah <aditia.pikarin@gmail.com>
2021-10-24 18:09:40 +0700

    Updates

---
+++ _nuxt/86dc623.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[37],{268:function(n,e,t){"use strict";t.r(e);var l={name:"Qualification"},c=t(37),component=Object(c.a)(l,(function(){var n=this.$createElement;return(this._self._c||n)("div")}),[],!1,null,null,null);e.default=component.exports}}]);
