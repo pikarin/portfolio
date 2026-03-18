e7be55dc33e3264ea34ca08c949778bc1f63cea7
M. Aditia Firmansyah <aditia.pikarin@gmail.com>
2026-03-18 17:48:29 +0700

    Rename static to public and create app.vue
    - Rename static/ to public/ (Nuxt 3 convention)
    - Create app.vue with NuxtPage and body classes via useHead

---
+++ app.vue
@@ -0,0 +1,11 @@
+<template>
+  <NuxtPage />
+</template>
+
+<script setup>
+useHead({
+  bodyAttrs: {
+    class: 'font-poppins antialiased mb-12 bg-body text-default lg:bg-gray-100',
+  },
+})
+</script>
